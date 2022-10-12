83ccdf845206f6bdcb8137d3c9ec3e931f94fadb
3dfx <ad_ar@gmx.de>
2022-10-12 19:07:52 +0200

    Added hoverpic for guest-stars in episode-details

--- template/js/hoverpic.js
+++ template/js/hoverpic.js
@@ -5,12 +5,13 @@ jQuery(document).ready(function() {
 	yHoverPicOffset = 220;
 	enterPos = null;
 	enterMoved = false;
+
 	$("a.hoverpic").hover(
 		function(e) {
 			this.t = this.title;
 			this.title = "";
 
-			var c = (this.t != "") ? "<br/>" + this.t : "";
+			var c = (this.t !== "") ? "<br/>" + this.t : "";
 			$("body").append("<p id='hoverpic'><img src='"+ this.rel +"'/>"+ c +"</p>");
 			$("#hoverpic")
 				.css("top",(e.pageY - correctYoffset(e, $(this).offset().top)) + "px")
@@ -23,7 +24,8 @@ jQuery(document).ready(function() {
 			$("#hoverpic").remove();
 		}
 	);
-	$("a.hoverpic").mousemove(function(e) {
+
+	$(`a.hoverpic`).mousemove(function(e) {
 		enterMoved = true;
 		$("#hoverpic")
 			.css("top",(e.pageY - correctYoffset(e, $(this).offset().top)) + "px")
@@ -37,16 +39,14 @@ jQuery(document).ready(function() {
 		if (enterPos == null) { enterPos = $("#hoverpic").offset().top; }
 		var val  = enterPos - (offsetTop - CWOffset);
 		var val2 = CWOffset - val;
-		var hHeight = $("#hoverpic").height();
+		var hHeight = $(`#hoverpic`).height();
 
+		// noinspection EqualityComparisonWithCoercionJS
 		if (hHeight == 0) { res = 138/2; }
 		else if (CWOffset - val  < hHeight) { res = 0; }
 		else if (CWOffset - val2 > hHeight) { res = hHeight / 2; }
 		else { res = hHeight; }
 
-		/*if (offsetTop - enterPos < $("#hoverpic").height()) { console.log('a'); res = 0; }
-		else if (CWOffset - offsetTop - enterPos < 150) { console.log('b'); res = $("#hoverpic").height(); }
-		else if (CWOffset > yHoverPicOffset) { console.log('c'); res = 90; }*/
 		return res;
 	}
 });
